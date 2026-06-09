eebacb6b4d03f1951290a69bc042ec15f21f3b32
ajacint0 <157071572+ajacint0@users.noreply.github.com>
2026-06-09 17:18:57 -0400

    Update cellpose_test.py

--- cellpose_test.py
+++ cellpose_test.py
@@ -5,7 +5,7 @@ import os
 from glob import glob
 import re
 
-os.chdir('/mnt/home/ajacinto/ceph/tracked_embryos/2025-12-17_160948/new_test_cellpose/')
+os.chdir('/mnt/home/ajacinto/ceph/tracked_embryos/2025-12-17_160948/test_cellpose/')
 for f in sorted(glob('*.tif')):
 	num = re.findall('\d+', f)
 	num0 = int(num[0])
@@ -13,4 +13,4 @@ for f in sorted(glob('*.tif')):
 	img = tfl.imread(f)
 	model = models.Cellpose(gpu=True, model_type='cyto2')
 	masks, flows, styles, diams = model.eval(img, tile_overlap = .5, do_3D=True,anisotropy = 1.0, tile=True, min_size=128, batch_size=8)
-	tfl.imwrite(f'/mnt/home/ajacinto/ceph/tracked_embryos/2025-12-17_160948/new_cellpose/cropped_raw_ch_{num0}_tp_{num1}_cp_masks.tif', masks.astype(np.uint16))
+	tfl.imwrite(f'/mnt/home/ajacinto/ceph/tracked_embryos/2025-12-17_160948/cellpose/cropped_raw_ch_{num0}_tp_{num1}_cp_masks.tif', masks.astype(np.uint16))
