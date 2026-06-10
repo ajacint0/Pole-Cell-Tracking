b47d33a8d90d53577f5ecbf9f4396cb72bc4fcfa
ajacint0 <157071572+ajacint0@users.noreply.github.com>
2026-06-10 16:04:11 -0400

    Update build_tree_from_match_files.py

--- build_tree_from_match_files.py
+++ build_tree_from_match_files.py
@@ -34,8 +34,8 @@ for i in timevect:
 	
 
 
-	npy_pairs = np.load(f'/mnt/home/ajacinto/ceph/tracked_embryos/{movie}/matches/matches_{i}_store_matches_new.npy')
-	npy_pairs_scores = np.load(f'/mnt/home/ajacinto/ceph/tracked_embryos/{movie}/matches/matches_{i}_store_match_scores_new.npy')
+	npy_pairs = np.load(f'/mnt/home/ajacinto/ceph/tracked_embryos/{movie}/matches/matches_{i}_store_matches.npy')
+	npy_pairs_scores = np.load(f'/mnt/home/ajacinto/ceph/tracked_embryos/{movie}/matches/matches_{i}_store_match_scores.npy')
 
 
 	#if i == 30:
