0a7a23726c78ccae70a49b545d243b6115575ffe
ajacint0 <157071572+ajacint0@users.noreply.github.com>
2026-06-26 12:21:54 -0400

    Update add_nodes.py

--- add_nodes.py
+++ add_nodes.py
@@ -2,18 +2,24 @@ from skimage.measure import label, regionprops
 import numpy as np
 import tifffile as tfl
 import csv
+from glob import glob
+import re
+import os
 
-nuclear_seg = '/mnt/home/hnunley/ceph/tracked_embryos/2025-07-11_121341/gui_segs/tp_11_ch_1_seg.tif'
-tp = 11
-img = tfl.imread(nuclear_seg)
-nuclei_list = []
-nuclei = regionprops(img)
-for nucleus in nuclei:
-	nuclei_list.append(f'{tp:03}_{nucleus.label:03}')
-print(nuclei_list)
-with open(f'/mnt/home/ajacinto/ceph/tracked_embryos/2025-07-11_121341/csvs/graph.csv', 'a', newline='') as f:
-			writer = csv.writer(f)
-			for nuc in nuclei_list:
-			
-				writer.writerow([nuc,'',''])
+os.chdir('/mnt/home/hnunley/ceph/tracked_embryos/2025-07-11_121341/gui_segs/')
+for f in sorted(glob('*.tif')):
+	num = re.findall('\d+', f)
+	num0 = int(num[0])
+	num1 = int(num[1])
 
+	img = tfl.imread(f)
+	nuclei_list = []
+	nuclei = regionprops(img)
+	for nucleus in nuclei:
+		nuclei_list.append(f'{num0:03}_{nucleus.label:03}')
+	print(nuclei_list)
+	with open(f'/mnt/home/ajacinto/ceph/tracked_embryos/2025-07-11_121341/csvs/graph.csv', 'a', newline='') as f:
+				writer = csv.writer(f)
+				for nuc in nuclei_list:
+				
+					writer.writerow([nuc,'',''])
